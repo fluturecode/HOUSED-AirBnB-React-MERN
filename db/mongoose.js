dc1f23488171fc301ee0122eea7b7fbb3d7cacc2
Elizabeth Eidelson <fluturecode@gmail.com>
2020-06-09 15:51:25 -0400

    moved finall changes for first commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,13 +4,6 @@ const User = require('../models/user');
 const mongoose = require('mongoose');
 // const User = require('../models/user');
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
-
 //this is just to test our models with compass/mongodb
 const me = new User({
   name: 'Liz',

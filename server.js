1bff947c0ed3b6aa75342f3020a3eb8d6b2368d4
Elizabeth Eidelson <59614789+fluturecode@users.noreply.github.com>
2020-06-13 23:38:25 -0400

    Updated server.js
    Add the booking route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const listingRoutes = require('./routes/listings')
 const userRoutes = require('./routes/users')
+const bookingRoutes = require('./routes/bookings')
 const mongoose = require('mongoose')
 require('./db/mongoose');
 
@@ -9,8 +10,9 @@ const port = process.env.PORT || 8080;
 
 
 app.use(express.json());
-app.use(listingRoutes)
 
+app.use(listingRoutes)
+app.use(bookingRoutes)
 app.use(userRoutes)
 
 app.listen(port, () => {

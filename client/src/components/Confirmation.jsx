a959089c3d24c136c878f1157baa37c2332702f5
albertpalacio <you@example.com>
2020-06-19 18:25:14 -0400

    extra fixes

--- client/src/components/Confirmation.jsx
+++ client/src/components/Confirmation.jsx
@@ -10,10 +10,10 @@ export default () => {
 
   return (
     <div className="conf-main">
-      <img src={require('../images/Image Verify Info.png')} alt="Celebration for reserving" />
+      <img  src={require('../images/Image Verify Info.png')} alt="Celebration for reserving" />
       <h2 className="header"> Reservation Sent!</h2>
       Thanks for inquiring, {user.firstName}(Guest name!)!
-    <p>  Your request for more information about the  <p className="host">{booking.listing_id.city}</p> is being sent to: <p className="host">{booking.listing_id.firstName}.</p> </p>
+    <p>  Your request for more information about the  <p className="host">{booking.listing_id.city}</p> is being sent to: <p className="host">{booking.listing_id.firstName}</p> </p>
     
     
       <p>Thanks for requesting more information on your booking:</p> 
@@ -22,10 +22,9 @@ export default () => {
 
 
       <h4 className="header"> What To Expect:</h4>
-      <h6>
-        Now that you have sent your request the host will get notified and be able to review
-        your application and decide. The followings things should be kept in mind:
-      </h6>
+      <h5>
+        Now that you have sent your request The followings things should be kept in mind:
+      </h5>
      
      <p>   1. Host will contact you first.</p>
      <p>  2. You and {booking.listing_id.firstName} (your host) will agree on the correct terms.</p>

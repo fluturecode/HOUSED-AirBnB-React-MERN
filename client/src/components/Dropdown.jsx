a98fbdcd8e5c29e496516cc67170a25a9ba09e99
Elizabeth Eidelson <59614789+fluturecode@users.noreply.github.com>
2020-06-20 00:09:13 -0400

    Update Dropdown.jsx
    We are not using this feature just yet.

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function Dropdown({ title, items = [], multiselect = false }) {
-  const [open, setOpen] = useState(false);
-  const [selection, setSelection] = useState([]);
-  const toggle = () => setOpen(!open);
+// function Dropdown({ title, items = [], multiselect = false }) {
+//   const [open, setOpen] = useState(false);
+//   const [selection, setSelection] = useState([]);
+//   const toggle = () => setOpen(!open);
 
-  const handleOnClick = () => {};
+//   const handleOnClick = () => {};
 
-  return (
-    <div className="dd-wrapper">
-      <div
-        tabIndex={0}
-        className="dd-header"
-        role="button"
-        onKeyPress={() => toggle(!open)}
-        onClick={() => toggle(!open)}
-      >
-        <div className="dd-header_title">
-          <p className="dd-header_title--bold">(title)</p>
-        </div>
-        <div className="dd-header_action">
-          <p>{open ? 'Close' : 'Open'}</p>
-        </div>
-      </div>
-      {open && (
-        <ul className="dd-list">
-          {items.map((item) => (
-            <li className="dd-list-item" key={item.id}>
-              <button type="button" onClick={() => handleOnClick(item)}>
-                <span>{item.value}</span>
-                <span></span>
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div className="dd-wrapper">
+//       <div
+//         tabIndex={0}
+//         className="dd-header"
+//         role="button"
+//         onKeyPress={() => toggle(!open)}
+//         onClick={() => toggle(!open)}
+//       >
+//         <div className="dd-header_title">
+//           <p className="dd-header_title--bold">(title)</p>
+//         </div>
+//         <div className="dd-header_action">
+//           <p>{open ? 'Close' : 'Open'}</p>
+//         </div>
+//       </div>
+//       {open && (
+//         <ul className="dd-list">
+//           {items.map((item) => (
+//             <li className="dd-list-item" key={item.id}>
+//               <button type="button" onClick={() => handleOnClick(item)}>
+//                 <span>{item.value}</span>
+//                 <span></span>
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
 
-export default Dropdown;
+// export default Dropdown;

71bff59cf5816d520194e8e17393521e86a41ba8
Elizabeth Eidelson <fluturecode@gmail.com>
2020-06-19 23:36:17 -0400

    added homepage icons

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import '../App.css';
+import WOW from 'wowjs';
 
-const HowItWorks = () => {
-  return (
-    <div class="howItWorks">
-      <div class="main-img">
-        <img
-          src={require('../images/Image Verify Info.png')}
-          alt="Two people high-fiving"
-        />
+class HowItWorks extends React.Component {
+  componentDidMount() {
+    new WOW.WOW().init();
+  }
+  render() {
+    return (
+      <div class="howItWorks">
+        <div class="main-img">
+          <img
+            src={require('../images/Image Verify Info.png')}
+            alt="Two people high-fiving"
+          />
+        </div>
+        <div class="steps">
+          <ol>
+            <li
+              className="wow slideInLeft"
+              data-wow-duration="2s"
+              data-wow-delay="2s"
+            >
+              Step 1
+            </li>
+            <p></p>
+            <li
+              className="wow slideInLeft"
+              data-wow-duration="2s"
+              data-wow-delay="4s"
+            >
+              Step 2
+            </li>
+            <p></p>
+            <li
+              className="wow slideInLeft"
+              data-wow-duration="2s"
+              data-wow-delay="6s"
+            >
+              Step 3
+            </li>
+            <p></p>
+          </ol>
+        </div>
       </div>
-      <div class="steps">
-        <ol>
-          <li>Step 1</li>
-          <p></p>
-          <li>Step 2</li>
-          <p></p>
-          <li>Step 3</li>
-          <p></p>
-        </ol>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default HowItWorks;

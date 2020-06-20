e29489ad4f444a5f0163ecab09620f1c5164be10
albertpalacio <albertpalacio@outlook.com>
2020-06-20 15:43:07 -0400

    styling updated for host and safety

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,7 @@ import CustomChatbot from './chatbot/CustomChatbot';
 import bgcheck from '../images/bgcheck.png';
 import neighborhood from '../images/neighborhood.jpg';
 import membericon from '../images/membericon.jpg';
+import becomehost from '../images/becomehost.png';
 
 const Home = () => {
   return (
@@ -18,8 +19,8 @@ const Home = () => {
       </div>
 
       <div className="safety-list">
-        <h2> We Take Safety Seriously!</h2>
-        <div className="safety-steps">
+        <h1 className="header"> We Take Safety Seriously!</h1>
+        <div className="safety-background">
           <img src={bgcheck} alt="background check" />
           <h2 className="header"> Background Checks</h2>
           <p className="pinfo">We believe your safety is everything.</p>
@@ -28,7 +29,7 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="safety-steps">
+        <div className="safety-neighborhood">
           <img src={neighborhood} alt="neighborhood checks" />
           <h2 className="header"> Neighborhood Checks</h2>
           <p className="pinfo">
@@ -39,7 +40,7 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="safety-steps">
+        <div className="safety-badges">
           <img src={membericon} alt="Member icons" />
           <h2 className="header"> Achievement Badges</h2>
           <p className="pinfo">
@@ -48,7 +49,7 @@ const Home = () => {
         </div>
       </div>
       <div className="become-host">
-        <h2> Become a Host!</h2>
+        <img className="hostimage" src={becomehost} alt="become host link" />
         <CustomChatbot />
       </div>
     </div>

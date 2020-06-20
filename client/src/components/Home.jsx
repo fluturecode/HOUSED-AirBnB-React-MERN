26f102b7b11b9eea5148ee1baa20c15b62a2182b
albertpalacio <albertpalacio@outlook.com>
2020-06-20 14:38:42 -0400

    safety styling done

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -21,17 +21,30 @@ const Home = () => {
         <h2> We Take Safety Seriously!</h2>
         <div className="safety-steps">
           <img src={bgcheck} alt="background check" />
-          <i class="fas fa-user-shield icon-2x" />
+          <h2 className="header"> Background Checks</h2>
+          <p className="pinfo">We believe your safety is everything.</p>
+          <p className="pinfo">
+            We run checks to make sure each person is verified and approved.
+          </p>
         </div>
 
         <div className="safety-steps">
           <img src={neighborhood} alt="neighborhood checks" />
-          <i class="fas fa-user-shield icon-2x"></i>
+          <h2 className="header"> Neighborhood Checks</h2>
+          <p className="pinfo">
+            We give you a report of the area you will be staying
+          </p>
+          <p className="pinfo">
+            before you confirm to make sure you know where you are going.{' '}
+          </p>
         </div>
 
         <div className="safety-steps">
           <img src={membericon} alt="Member icons" />
-          <i class="fas fa-user-shield icon-2x"></i>
+          <h2 className="header"> Achievement Badges</h2>
+          <p className="pinfo">
+            We designate icons to hosts and guests as they reach milestones!
+          </p>
         </div>
       </div>
       <div className="become-host">

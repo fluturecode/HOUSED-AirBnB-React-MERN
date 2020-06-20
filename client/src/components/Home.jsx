9c013c8dcf38ac49b6f1a989f4fc54e4c031cd65
albertpalacio <albertpalacio@outlook.com>
2020-06-20 13:08:16 -0400

    started safety icons and styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../styles/home.css';
 import Search from './Search';
 import CustomChatbot from './chatbot/CustomChatbot';
+import bgcheck from '../images/bgcheck.png';
+import neighborhood from '../images/neighborhood.jpg';
+import membericon from '../images/membericon.jpg';
 
 const Home = () => {
   return (
@@ -9,21 +12,26 @@ const Home = () => {
       <div className="top-img">
         <img src={require('../images/cozy_room.jpg')} alt="Cozy bedroom" />
       </div>
+
       <div className="search-container">
         <Search />
       </div>
+
       <div className="safety-list">
         <h2> We Take Safety Seriously!</h2>
         <div className="safety-steps">
-          step 1 <i class="fas fa-user-shield icon-2x"></i>
+          <img src={bgcheck} alt="background check" />
+          <i class="fas fa-user-shield icon-2x" />
         </div>
+
         <div className="safety-steps">
-          {' '}
-          step 2<i class="fas fa-user-shield icon-2x"></i>
+          <img src={neighborhood} alt="neighborhood checks" />
+          <i class="fas fa-user-shield icon-2x"></i>
         </div>
+
         <div className="safety-steps">
-          {' '}
-          step 3<i class="fas fa-user-shield icon-2x"></i>
+          <img src={membericon} alt="Member icons" />
+          <i class="fas fa-user-shield icon-2x"></i>
         </div>
       </div>
       <div className="become-host">

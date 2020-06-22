7bb0ad44b285967dcff2243146444e076bd82b9b
Elizabeth Eidelson <59614789+fluturecode@users.noreply.github.com>
2020-06-22 18:50:05 -0400

    Update how it works
    Workding and typos.

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -15,10 +15,10 @@ class HowItWorks extends React.Component {
           <div class="steps">
             <h1> How It Works</h1>
             <h4>Step 1</h4>
-            <p> Search our listings, and select a place.</p>
+            <p> Type in a city and a month that you are looking for a place to stay, and search our available listings. </p>
             <h4>Step 2</h4>
             <p>
-              Fill out your profile, add your preferences, and upload your ID.
+              Select a place, then fill out your profile, add your preferences, and upload your ID.
             </p>
             <h4>Step 3</h4>
             <p>

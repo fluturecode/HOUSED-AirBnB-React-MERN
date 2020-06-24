4cc91e7c3084d97bf2c14b774bc7ce0d63ff356f
Elizabeth Eidelson <59614789+fluturecode@users.noreply.github.com>
2020-06-23 20:01:13 -0400

    Fixed spelling error.

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -15,14 +15,14 @@ class HowItWorks extends React.Component {
           <div class="steps">
             <h1> How It Works</h1>
             <h4>Step 1</h4>
-            <p> Type in a city and a month that you are looking for a place to stay, and search our available listings. </p>
+            <p> Type in a city and the month that you are looking for a place to stay, and search our available listings. </p>
             <h4>Step 2</h4>
             <p>
-              Select a place, then fill out your profile, add your preferences, and upload your ID.
+              Select a place, fill out your profile, add your preferences, and upload your ID.
             </p>
             <h4>Step 3</h4>
             <p>
-              You will get a confirmation, and when we will connect you with the hose and your new home!
+              You will receive a confirmation, and we will connect you with your host and your new home!
             </p>
           </div>
           <img

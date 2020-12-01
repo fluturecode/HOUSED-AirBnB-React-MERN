89a2c52673348bbcf994d4230538ee15c8b09fb9
Elizabeth Eidelson <fluturecode@gmail.com>
2020-12-01 16:25:00 -0500

    updated homepage.js

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import { Button } from '@material-ui/core';
+import Search from './Search.js';
+import { useHistory } from 'react-router-dom';
+import homeImage from './homeImage.jpg';
 
-export const Banner = () => {
+function Banner() {
+  const history = useHistory();
+  const [showSearch, setShowSearch] = useState(false);
+
+  const BannerContainer = styled.div`
+    background-image: url('${homeImage}');
+    background-position: center center;
+    height: 80vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+  `;
+
+  const BannerSearch = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const BannerInfo = styled.div`
+    background-color: rgb(66, 70, 70);
+    color: white;
+    padding-top: 14vh;
+    padding-left: 50px;
+    padding-right: 50px;
+    padding-bottom: 50px;
+    width: 280px;
+    border-radius: 10px;
+  `;
+
   return (
-    <div>
-      <h1>I am the Banner</h1>
-    </div>
-  )
+    <>
+      <BannerContainer>
+        <BannerSearch>
+          {showSearch && <Search />}
+          <Button
+            onClick={() => setShowSearch(!showSearch)}
+            className="banner__searchButton"
+            variant="outlined"
+          >
+            {showSearch ? 'Hide' : 'Search Dates'}
+          </Button>
+        </BannerSearch>
+        <BannerInfo>
+          <h1>Discover your next home away from home...</h1>
+          <h5>
+            Plan a different kind of getaway to uncover the hidden gems near you
+          </h5>
+          <Button onClick={() => history.push('/search')} variant="outlined">
+            Explore Nearby
+          </Button>
+        </BannerInfo>
+      </BannerContainer>
+    </>
+  );
 }
+
+export default Banner;

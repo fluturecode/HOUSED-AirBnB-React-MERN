9dda08ab4d1a1c6c87f140b299d4777cd33919cd
Elizabeth Eidelson <fluturecode@gmail.com>
2020-12-01 20:08:18 -0500

    fixed Banner.js

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -16,25 +16,42 @@ function Banner() {
     background-repeat: no-repeat;
     background-size: cover;
     position: relative;
-    margin-left: 80px;
-    margin-right: 80px;
-  `;
-
-  const BannerSearch = styled.div`
-    display: flex;
-    flex-direction: column;
   `;
 
   const BannerInfo = styled.div`
-    background-color: grey;
-    color: white;
-    padding-top: 14vh;
-    padding-left: 50px;
-    padding-right: 50px;
-    padding-bottom: 50px;
-    width: 280px;
-    border-radius: 10px;
-  `;
+      background-color: grey;
+      color: white;
+      padding-top: 10vh;
+      padding-left: 50px;
+      padding-right: 50px;
+      padding-bottom: 10vh;
+      width: 300px;
+      border-radius: 10px;
+    `,
+    button = styled.button`
+      background-color: #ff7779;
+      color: white;
+      text-transform: inherit;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      font-weight: 600;
+      &:hover {
+        background-color: white;
+        color: #ff7779;
+      }
+    `,
+    h5 = styled.div`
+      margin-top: 10px;
+    `;
+
+  const BannerSearch = styled.div`
+      display: flex;
+      flex-direction: column;
+    `,
+    ExploreButton = styled.button`
+      font-weight: 900 !important;
+      text-transform: inherit !important;
+    `;
 
   return (
     <>
@@ -50,9 +67,12 @@ function Banner() {
           <h5>
             Plan a different kind of getaway to uncover the hidden gems near you
           </h5>
-          <Button onClick={() => history.push('/search')} variant="outlined">
+          <ExploreButton
+            onClick={() => history.push('/search')}
+            variant="outlined"
+          >
             Explore Nearby
-          </Button>
+          </ExploreButton>
         </BannerInfo>
       </BannerContainer>
     </>
